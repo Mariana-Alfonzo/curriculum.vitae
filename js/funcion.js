59276023736ad8aae56210cdfaf15bdaaefd59a7
Mariana Alfonzo <marianaalfonzo@yahoo.com>
2023-03-08 18:59:57 -0300

    Practicando JavaScript

---
+++ js/funcion.js
@@ -0,0 +1,4 @@
+document.getElementById('boton_contacto').addEventListener("click", function(){
+    console.log("Datos de contacto");
+    document.getElementById('contactame').innerText = "(55)1234-5678"
+})
